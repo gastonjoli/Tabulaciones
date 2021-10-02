5eaec7a2aa60bc60e46136e680936785686e3359
Falso <falsee63@gmail.com>
2021-10-02 09:40:51 -0300

    Tabulaciones JAVA

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,12 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+      public static void main(String[] args) {
+        System.out.println("Hola Mundo, Me llamo Gaston");
+        System.out.println("Estoy Practicando java");
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Gaston";
+        var apellido = "Jolinont";
+
+        System.out.println(nombre +" "+ apellido);
+        System.out.println("Nueva linea: \n"+ nombre);
+        System.out.println("Tabulador: \t"+ nombre);
+        System.out.println("Retroceso: \b"+ nombre);
+        System.out.println("Retorno de carro: "+ nombre);
+        System.out.println("Comilla simple: '" + nombre + "'");
+        System.out.println("Comilla doble \""+ nombre + "\"");
+        System.out.println();
+        System.out.println(nombre);
+
+
+    }
+}
